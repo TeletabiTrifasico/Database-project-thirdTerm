ea063e0cf7a0f2165e8816c32259b789eb4b9f14
Tarik <ahmettariknl@gmail.com>
2024-03-13 15:13:02 +0100

    Update Program.cs

--- SomerenUI/Program.cs
+++ SomerenUI/Program.cs
@@ -10,6 +10,9 @@ namespace SomerenUI
         /// </summary>
         [STAThread]
         //example
+        //
+        //
+
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
