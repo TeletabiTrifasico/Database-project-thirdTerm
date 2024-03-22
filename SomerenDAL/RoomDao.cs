376c7e737d1e9d691fbf4b00b97630ca06353e2f
Ray-Allen Benjamin <104226630+MaximusAurelias@users.noreply.github.com>
2024-03-22 14:08:06 +0100

    RoomDAO,RoomService

---
+++ SomerenDAL/RoomDao.cs
@@ -0,0 +1,42 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SomerenDAL
+{
+    public class RoomDao : BaseDao
+    {
+        
+            public List<Room> GetAllRooms()
+            {
+                string query = "SELECT * FROM room";
+                SqlParameter[] sqlParameters = new SqlParameter[0];
+                return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+            }
+
+            private List<Room> ReadTables(DataTable dataTable)
+            {
+                List<Room> rooms = new List<Room>();
+
+                foreach (DataRow dr in dataTable.Rows)
+                {
+                    Room room = new Room()
+                    {
+                        Id = (int)dr["roomID"],
+                        Number = (int)dr["floornumber"],
+                        Size = (int)dr["capacity"],
+                        Type = (string)dr["building"]
+                    };
+                    rooms.Add(room);
+                }
+                return rooms;
+            }
+    }
+
+}
+
---
+++ SomerenService/RoomService.cs
@@ -0,0 +1,28 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomerenDAL;
+
+namespace SomerenService
+{
+    public class RoomService
+    {
+        
+            private RoomDao roomdb;
+
+            public RoomService()
+            {
+                roomdb = new RoomDao();
+            }
+
+            public List<Room> GetRooms()
+            {
+                List<Room> rooms = roomdb.GetAllRooms();
+                return rooms;
+            }
+        
+    }
+}
