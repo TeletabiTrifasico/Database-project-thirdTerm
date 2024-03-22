117496b6b0c72194fcf7963061b459cb040a85a4
Ray-Allen Benjamin <104226630+MaximusAurelias@users.noreply.github.com>
2024-03-22 14:11:47 +0100

    Sommerin UI change

--- SomerenUI/SomerenUI.cs
+++ SomerenUI/SomerenUI.cs
@@ -49,6 +49,56 @@ namespace SomerenUI
                 MessageBox.Show("Something went wrong while loading the students: " + e.Message);
             }
         }
+        private void ShowRoomsPanel()
+        {
+            pnlDashboard.Hide();
+
+            pnlStudents.Hide();
+
+            pnlRooms.Show();
+
+            try
+            {
+                List<Room> rooms = GetRooms();
+                DisplayRooms(rooms);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong while loading the rooms: " + e.Message);
+
+            }
+        }
+
+
+
+        private List<Room> GetRooms()
+        {
+            RoomService roomService = new RoomService();
+            List<Room> rooms = roomService.GetRooms();
+            return rooms;
+
+        }
+
+        private void DisplayRooms(List<Room> rooms)
+        {
+            // clear the listview before filling it
+
+            listViewRooms.Items.Clear();
+
+            foreach (Room room in rooms)
+            {
+                ListViewItem li = new ListViewItem(room.Id.ToString());
+                li.SubItems.Add(room.Number.ToString());
+                li.SubItems.Add(room.Size.ToString());
+                li.SubItems.Add(room.Type.ToString());
+                //Comment or something anywhere
+                li.Tag = room;   // link room object to listview item
+                listViewRooms.Items.Add(li);
+
+            }
+        }
+
+
 
         private List<Student> GetStudents()
         {
